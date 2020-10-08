abf8bb431c36a87b6895c19d1e23d49c5e49107f
ZachSiapno <EWSB52@gmail.com>
2020-10-08 19:40:25 -0400

    Updated Code

--- QuestionApp (FirstGitHub)/ViewController.swift
+++ QuestionApp (FirstGitHub)/ViewController.swift
@@ -8,15 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        questionLabel.numberOfLines = 0
+        answerLabel.numberOfLines = 0
+        answerLabel.text = "Make sure to capitalize the first letter of your birth month"
         view.backgroundColor = UIColor.green
     }
 
-
+    @IBAction func answerButton(_ sender: UIButton)
+    {
+        if questionTextField.text! == "January"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "February"
+        {
+            answerLabel.text = "False"
+        }
+        else if questionTextField.text! == "March"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "April"
+        {
+            answerLabel.text = "False"
+        }
+        else if questionTextField.text! == "May"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "June"
+        {
+            answerLabel.text = "False"
+        }
+        else if questionTextField.text! == "July"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "August"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "September"
+        {
+            answerLabel.text = "False"
+        }
+        else if questionTextField.text! == "October"
+        {
+            answerLabel.text = "True"
+        }
+        else if questionTextField.text! == "November"
+        {
+            answerLabel.text = "False"
+        }
+        else if questionTextField.text! == "December"
+        {
+            answerLabel.text = "True"
+        }
+    }
+    
 }
 
